9e1398eac021de5292453b130acf5aaa020904ff
ymarselino <m11202819@gapps.ntust.edu.tw>
2025-08-27 00:08:21 +0800

    Add k8s cluster management function:
    - Add Readme.md under sol003
    - Register k8s cluster
    - Authenticate k8s cluster by upload the base64 kubeconfig <TMP Solution>
    
    sol003 function:
    - Create Descriptor
    - Create Deployment
    - Instantiate Deployment
    - Fix Termination
    - Fix expansion of dot notation for helm values
    
    Change-Id: Ibb7298c482d494107244a5c222a6f0fb6efb1ecd
    Signed-off-by: ymarselino <m11202819@gapps.ntust.edu.tw>

--- nfo/k8s/nfo_microservice/settings.py
+++ nfo/k8s/nfo_microservice/settings.py
@@ -18,14 +18,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ' '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.8.35']
 
 
 # Application definition
@@ -37,9 +36,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'rest_framework',
     'helm_processor',
     'packagevariant_processor',
+    'sol003',
 ]
 
 MIDDLEWARE = [
